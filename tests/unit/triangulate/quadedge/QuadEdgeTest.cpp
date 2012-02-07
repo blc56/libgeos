dd7fd72a112bd5fd09934e3654e46676c7c64526
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-02-07 16:04:34 -0600

    Port of DelaunayTriangulation:
     * add test for QuadEdge::swap()

--- tests/unit/triangulate/quadedge/QuadEdgeTest.cpp
+++ tests/unit/triangulate/quadedge/QuadEdgeTest.cpp
@@ -100,6 +100,74 @@ namespace tut
 		delete r0; delete r1; delete r2; delete r3;
 		delete s0; delete s1; delete s2; delete s3;
 	}
+
+	// 3 - QuadEdge::swap()
+	template<>
+	template<>
+	void object::test<3>()
+	{		 
+		Vertex v1(0, 0);
+		Vertex v2(0, 1);
+
+		Vertex v3(1, 0);
+		Vertex v4(1, 1);
+
+		QuadEdge *q0, *q1, *q2, *q3;
+		QuadEdge *r0, *r1, *r2, *r3;
+		QuadEdge *s0, *s1, *s2, *s3;
+		QuadEdge *t0, *t1, *t2, *t3;
+		QuadEdge *u0, *u1, *u2, *u3;
+
+		//make a quadilateral
+		QuadEdge::makeEdge(v1, v2, &q0, &q1, &q2, &q3);
+		QuadEdge::makeEdge(v4, v3, &r0, &r1, &r2, &r3);
+		QuadEdge::connect(*q0, *r0, &s0, &s1, &s2, &s3);
+		QuadEdge::connect(*r0, *q0, &t0, &t1, &t2, &t3);
+
+		//printf("\n=====================\n");
+		//printf("r0->orig(): %f %f\n", r0->orig().getX(), r0->orig().getY());
+		//printf("r0->dest(): %f %f\n", r0->dest().getX(), r0->dest().getY());
+		//printf("s0->orig(): %f %f\n", s0->orig().getX(), s0->orig().getY());
+		//printf("s0->dest(): %f %f\n", s0->dest().getX(), s0->dest().getY());
+
+		//add an interior edge to make 2 triangles
+		QuadEdge::connect(*t0, *r0, &u0, &u1, &u2, &u3);
+		//printf("\n=====================\n");
+		//printf("q0->orig(): %f %f\n", q0->orig().getX(), q0->orig().getY());
+		//printf("q0->dest(): %f %f\n", q0->dest().getX(), q0->dest().getY());
+		//printf("r0->orig(): %f %f\n", r0->orig().getX(), r0->orig().getY());
+		//printf("r0->dest(): %f %f\n", r0->dest().getX(), r0->dest().getY());
+		//printf("s0->orig(): %f %f\n", s0->orig().getX(), s0->orig().getY());
+		//printf("s0->dest(): %f %f\n", s0->dest().getX(), s0->dest().getY());
+		//printf("t0->orig(): %f %f\n", t0->orig().getX(), t0->orig().getY());
+		//printf("t0->dest(): %f %f\n", t0->dest().getX(), t0->dest().getY());
+		//printf("u0->orig(): %f %f\n", u0->orig().getX(), u0->orig().getY());
+		//printf("u0->dest(): %f %f\n", u0->dest().getX(), u0->dest().getY());
+		ensure(t0->dest().equals(u0->orig()));
+		ensure(u0->dest().equals(r0->orig()));
+
+		//now swap the interior edge
+		QuadEdge::swap(*u0);
+		//printf("\n=====================\n");
+		//printf("q0->orig(): %f %f\n", q0->orig().getX(), q0->orig().getY());
+		//printf("q0->dest(): %f %f\n", q0->dest().getX(), q0->dest().getY());
+		//printf("r0->orig(): %f %f\n", r0->orig().getX(), r0->orig().getY());
+		//printf("r0->dest(): %f %f\n", r0->dest().getX(), r0->dest().getY());
+		//printf("s0->orig(): %f %f\n", s0->orig().getX(), s0->orig().getY());
+		//printf("s0->dest(): %f %f\n", s0->dest().getX(), s0->dest().getY());
+		//printf("t0->orig(): %f %f\n", t0->orig().getX(), t0->orig().getY());
+		//printf("t0->dest(): %f %f\n", t0->dest().getX(), t0->dest().getY());
+		//printf("u0->orig(): %f %f\n", u0->orig().getX(), u0->orig().getY());
+		//printf("u0->dest(): %f %f\n", u0->dest().getX(), u0->dest().getY());
+		ensure(r0->dest().equals(u0->dest()));
+		ensure(u0->orig().equals(q0->dest()));
+
+		delete q0; delete q1; delete q2; delete q3;
+		delete r0; delete r1; delete r2; delete r3;
+		delete s0; delete s1; delete s2; delete s3;
+		delete t0; delete t1; delete t2; delete t3;
+		delete u0; delete u1; delete u2; delete u3;
+	}
 } // namespace tut
 
 
